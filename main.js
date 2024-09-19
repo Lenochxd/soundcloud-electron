d432c509b75a714968c4bba4378d4f6b4b43feca
Lenoch <81lennoch@gmail.com>
2024-09-19 13:50:36 +0200

    Fix login issue by adding a valid useragent

--- main.js
+++ main.js
@@ -19,6 +19,10 @@ async function createWindow() {
         }
     });
 
+    // Set a valid user agent
+    const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36';
+    mainWindow.webContents.setUserAgent(userAgent);
+
     // Load SoundCloud
     mainWindow.loadURL('https://soundcloud.com');
 
